9f8dc6fcda0a3b1f71a139cd8d457343f83799dd
najun-auto <72716047+najun-auto@users.noreply.github.com>
2022-01-04 08:25:03 +0900

    21

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ import 'package:cli_script/cli_script.dart';
 
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:intl/intl.dart';
+import 'dart:math' as math;
+
+// https://pub.dev/packages/syncfusion_flutter_charts
 
 void main() {
   runApp(const MyApp());
@@ -41,14 +44,27 @@ class _MyHomePageState extends State<MyHomePage> {
   String gpu_usage_g21 = "cat /sys/devices/platform/18500000.mali/utilization";
   List<SalesData> _chartData = [];
 
+  late ChartSeriesController _chartSeriesController;
+
 
   @override
   void initState() {
 
-    _start();
+    // _start();
     _chartData = getChartData();
+    Timer.periodic(const Duration(seconds: 1), updateDataSource);
     super.initState();
 
+  }
+
+  double yeartmp = 2022;
+  void updateDataSource(Timer timer){
+    _chartData.add(SalesData(yeartmp++, (math.Random().nextInt(60) + 30)));
+    _chartData.removeAt(0);
+    _chartSeriesController.updateDataSource(
+      addedDataIndex: _chartData.length -1,
+      removedDataIndex: 0
+    );
   }
 
   List<SalesData> getChartData(){
@@ -63,20 +79,20 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
 
-  void _start(){
-    int temp = 0;
-    const oneSec = Duration(seconds:1);
-    Timer.periodic(
-      oneSec,
-      (Timer t) => setState(() {
-        wrapMain(() async {
-          var pipeline = Script(gpu_usage_g21);
-          kkk = await pipeline.stdout.text;
-          // print("${await pipeline.stdout.text} instances of waitFor");
-        });
-      }),
-    );
-  }
+  // void _start(){
+  //   int temp = 0;
+  //   const oneSec = Duration(seconds:1);
+  //   Timer.periodic(
+  //     oneSec,
+  //     (Timer t) => setState(() {
+  //       wrapMain(() async {
+  //         var pipeline = Script(gpu_usage_g21);
+  //         kkk = await pipeline.stdout.text;
+  //         // print("${await pipeline.stdout.text} instances of waitFor");
+  //       });
+  //     }),
+  //   );
+  // }
 
 
 
@@ -87,8 +103,20 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SfCartesianChart(
         title: ChartTitle(text: 'test'),
         legend: Legend(isVisible: true),
-        series: <ChartSeries>[
-          LineSeries<SalesData, double>(name: 'Sales', dataSource: _chartData, xValueMapper: (SalesData sales, _) => sales.year, yValueMapper: (SalesData sales, _) => sales.sales, dataLabelSettings: DataLabelSettings(isVisible: true))
+        series: <FastLineSeries>[
+          FastLineSeries<SalesData, double>(
+            onRendererCreated: (ChartSeriesController controller) {
+              _chartSeriesController = controller;
+            },
+            name: 'Sales',
+            dataSource: _chartData,
+            xValueMapper: (SalesData sales, _) => sales.year,
+            yValueMapper: (SalesData sales, _) => sales.sales,
+            dataLabelSettings: DataLabelSettings(isVisible: true),
+            color: Colors.orange,
+            width: 10,
+            opacity: 0.5,
+          )
         ],
         primaryXAxis: NumericAxis(edgeLabelPlacement: EdgeLabelPlacement.shift),
         primaryYAxis: NumericAxis(
