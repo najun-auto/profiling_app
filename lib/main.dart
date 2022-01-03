11ae211b7b6a5646d39c9ff319d8895bd4dd701d
najun-auto <72716047+najun-auto@users.noreply.github.com>
2022-01-03 15:32:56 +0900

    21

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:cli_script/cli_script.dart';
 
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -38,6 +39,28 @@ class _MyHomePageState extends State<MyHomePage> {
 
   String kkk = "1111";
   String gpu_usage_g21 = "cat /sys/devices/platform/18500000.mali/utilization";
+  List<SalesData> _chartData = [];
+
+
+  @override
+  void initState() {
+
+    _start();
+    _chartData = getChartData();
+    super.initState();
+
+  }
+
+  List<SalesData> getChartData(){
+    final List<SalesData> chartData = [
+      SalesData(2017, 25),
+      SalesData(2018, 12),
+      SalesData(2019, 24),
+      SalesData(2020, 18),
+      SalesData(2021, 30),
+    ];
+    return chartData;
+  }
 
 
   void _start(){
@@ -55,30 +78,31 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _start();
-  }
+
 
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      appBar: AppBar(
-
+    return SafeArea(child: Scaffold(
+      body: SfCartesianChart(
+        title: ChartTitle(text: 'test'),
+        legend: Legend(isVisible: true),
+        series: <ChartSeries>[
+          LineSeries<SalesData, double>(name: 'Sales', dataSource: _chartData, xValueMapper: (SalesData sales, _) => sales.year, yValueMapper: (SalesData sales, _) => sales.sales, dataLabelSettings: DataLabelSettings(isVisible: true))
+        ],
+        primaryXAxis: NumericAxis(edgeLabelPlacement: EdgeLabelPlacement.shift),
+        primaryYAxis: NumericAxis(
+            labelFormat: '{value}M',
+            numberFormat: NumberFormat.simpleCurrency(decimalDigits: 0)),
       ),
-      body: Container(
-        child: Text(kkk),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ));
   }
+
+
+}
+
+class SalesData{
+  SalesData(this.year, this.sales);
+  final double year;
+  final double sales;
 }
